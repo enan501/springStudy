eddab204260f26d1687d770b6c55b972927a3809
enan <hant2258@gmail.com>
2021-01-21 15:34:24 +0900

    Initial Commit

---
+++ src/main/java/com/backdev/happy/wblserver/WblServerApplication.java
@@ -0,0 +1,13 @@
+package com.backdev.happy.wblserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WblServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WblServerApplication.class, args);
+	}
+
+}
